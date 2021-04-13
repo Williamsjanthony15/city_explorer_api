9df885d9e11ecb39c7e7df825fa7630a147f30d5
Anthony Joel Williams <williamsjanthony15@yahoo.com>
2021-04-12 22:47:24 -0500

    lab review follow allong

---
+++ server.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+
+const weatherData = require('./data/weather.json');
+
+const app = express();
+app.use(cors());
+
+const PORT = process.env.PORT || 3001;
+
+
+//app.get method two arguments (endpoint, callback function(Can be multiple as well))
+app.get('/weather', (request, response) => {
+  try {
+  const allDailyForecasts = weatherData.data.map(day => new DailyForecast(day));
+  response.send('Looking for Weather?');
+} catch(error) {
+  handleErrors(error, response);
+}
+})
+
+function DailyForecast(day){
+  this.date = day.datetime;
+  this.description = day.weather.description;
+}
+
+function handleErrors (error, response) {
+  response.status(500).send('Internal Server Error');
+}
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
